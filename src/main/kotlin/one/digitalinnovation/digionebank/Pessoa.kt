ffb511938b6571686978b59a6ec7ccaaa602f355
JordaneMaurelli <jordane.maurelli@gmail.com>
2022-07-22 12:06:20 -0300

    Tornando o objeto CPF privado

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,11 +3,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Jordane"
     var cpf: String = "123.123.123-12"
+    private set
 }
 
 fun main(){
     val jordane = Pessoa()
 
+    println(jordane)
     println(jordane.nome)
     println(jordane.cpf)
 
