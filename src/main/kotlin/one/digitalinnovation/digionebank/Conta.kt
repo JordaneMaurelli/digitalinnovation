9368592b0b25206c8bd86874a3a56ca6ca0d8c14
jordanemaurelli <jordane.maurelli@gmail.com>
2022-07-27 18:37:52 -0300

    criação da classe Conta

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val conta: String,
+    val saldo:BigDecimal
+) {
+    fun deposit(valor: BigDecimal){
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}
